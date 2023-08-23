a0ee5ecab78666f7ee09ebac60f2133dba6f60fc
colspan <collin@collindigm.com>
2023-08-23 10:57:30 +0300

    Passing the function as a props and defined the function definition

--- src/component/Board.js
+++ src/component/Board.js
@@ -6,22 +6,28 @@ export const Board = () => {
 
     const [squares, setSquares] = useState(Array(9).fill(null)); // creates an array with nine elements and sets each of them to null.
 
+    const handleClick = () => {
+        const nextSquare = squares.slice();
+        nextSquare[0] = "X";
+        setSquares(nextSquare)
+    }
+
     return (
         <React.Fragment>
             <div className="board-row">
-                <Square value={squares[0]} />
-                <Square value={squares[1]} />
-                <Square value={squares[2]} />
+                <Square value={squares[0]} onSquareClick={handleClick} />
+                <Square value={squares[1]} onSquareClick={handleClick} />
+                <Square value={squares[2]} onSquareClick={handleClick} />
             </div>
             <div className="board-row">
-                <Square value={squares[3]} />
-                <Square value={squares[4]} />
-                <Square value={squares[5]} />
+                <Square value={squares[3]} onSquareClick={handleClick} />
+                <Square value={squares[4]} onSquareClick={handleClick} />
+                <Square value={squares[5]} onSquareClick={handleClick} />
             </div>
             <div className="board-row">
-                <Square value={squares[6]} />
-                <Square value={squares[7]} />
-                <Square value={squares[8]} />
+                <Square value={squares[6]} onSquareClick={handleClick} />
+                <Square value={squares[7]} onSquareClick={handleClick} />
+                <Square value={squares[8]} onSquareClick={handleClick} />
             </div>
         </React.Fragment>
     );
