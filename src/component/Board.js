0635ce4f1f20106c40ffa515fb74ad22c0b3fb6c
colspan <collin@collindigm.com>
2023-08-23 09:37:53 +0300

    Broke the Board into a Square component

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,22 +1,27 @@
 import React from "react";
 
+
+function Square() {
+    return <button className="square">1</button>
+}
+
 export const Board = () => {
     return (
         <React.Fragment>
             <div className="board-row">
-                <button className="square">1</button>
-                <button className="square">2</button>
-                <button className="square">3</button>
+                <Square />
+                <Square />
+                <Square />
             </div>
             <div className="board-row">
-                <button className="square">4</button>
-                <button className="square">5</button>
-                <button className="square">6</button>
+                <Square />
+                <Square />
+                <Square />
             </div>
             <div className="board-row">
-                <button className="square">7</button>
-                <button className="square">8</button>
-                <button className="square">9</button>
+                <Square />
+                <Square />
+                <Square />
             </div>
         </React.Fragment>
     );
