bdb846b3dba82f8ec158ad5a1bd4bb2389b602cd
colspan <collin@collindigm.com>
2023-08-23 09:49:05 +0300

    Passed the values via the props

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,27 +1,27 @@
 import React from "react";
 
 
-function Square() {
-    return <button className="square">1</button>
+function Square({ value }) {
+    return <button className="square">{value}</button>
 }
 
 export const Board = () => {
     return (
         <React.Fragment>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value="1" />
+                <Square value="2" />
+                <Square value="3" />
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value="4" />
+                <Square value="5" />
+                <Square value="6" />
             </div>
             <div className="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square value="7" />
+                <Square value="8" />
+                <Square value="9" />
             </div>
         </React.Fragment>
     );
