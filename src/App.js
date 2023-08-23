422f7eaf707d405d724d7aaecffe7140b047ff3d
colspan <collin@collindigm.com>
2023-08-23 08:45:04 +0300

    Consumed the square component into the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import {Square} from "./component/Square";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+          <Square />
       </header>
     </div>
   );
